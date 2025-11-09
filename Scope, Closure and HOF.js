78bb03f8e1a5e1a2edae3c8469dd4d3b6d378fa8
Muhammad Talha <TALHARIAZ750674@gmail.com>
2025-11-09 20:39:04 +0000

    Scope, Closure, HOF

--- Scope, Closure and HOF.js
+++ Scope, Closure and HOF.js
@@ -14,7 +14,7 @@
 //    SECOND  * Closure *
 
 
-/*
+
 function count(){
     let counter = 0; // local variable
     function increment(){
@@ -130,8 +130,6 @@ console.log(myAccount.withdraw());
 console.log(myAccount.getBalance());
 
 
-*/
-
 
 //    THIRD  * HOF (Higher Order Functions) *
 // A Higher Order Function is a function that either takes one or more
@@ -150,3 +148,47 @@ function double(){    // function to be passed as argument
 }
 let func = double();  // func now holds the inner function
 func();               // calling the inner function
+
+
+
+      // lexical scope example 1
+  // lexical scope mtlb aik function apny outer function k
+  //  variables ko access kr skta hai
+      
+
+function parent(){
+    let message = "Hello, World!";
+    function child(){
+        console.log(message);
+    }
+    child();
+}
+parent();
+
+
+     // example 2
+
+function papa(){
+    let papa = " I am Papa ";
+    function beta(){
+        console.log(papa);
+    }
+    beta();
+}
+papa();
+
+
+
+// example 3
+
+function outer(){
+    let a = " TALHA ";
+    let b = " Riaz ";
+    function inner(){
+        console.log(a + b);
+    }
+    return inner();
+}
+let func = outer;
+func();
+
